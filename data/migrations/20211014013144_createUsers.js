feea136be6b4fb41b16aa8893d979ae3a3197612
Kyle <kyle.e.cardwell@gmail.com>
2021-10-13 21:02:18 -0600

    users router and an error catcher

--- data/migrations/20211014013144_createUsers.js
+++ data/migrations/20211014013144_createUsers.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('users', table => {
+      table.increments('userId'),
+      table.string('username').notNullable().unique(),
+      table.string('password').notNullable()
+  })
+    
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users')
 };
--- data/seeds/00-clearData.js
+++ data/seeds/00-clearData.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {userId: 1, username: 'dCrockett', password: '1234'},
+        {userId: 2, username: 'pBill', password: '1234'},
+        {userId: 3, username: 'jHenry', password: '1234'}
       ]);
     });
 };
