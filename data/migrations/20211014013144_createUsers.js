dc5a502a028848c89b30412ff64262b0eb15eaf2
Kyle <kyle.e.cardwell@gmail.com>
2021-10-14 21:35:03 -0600

    added user and recipe table migrtions/seeds

--- data/migrations/20211014013144_createUsers.js
+++ data/migrations/20211014013144_createUsers.js
@@ -6,9 +6,41 @@ exports.up = function(knex) {
       table.string('username').notNullable().unique(),
       table.string('password').notNullable()
   })
+  .createTable('recipes', recipe => {
+      recipe.increments('recipeId'),
+      recipe.string('recipeName').notNullable(),
+      recipe.string('by').notNullable(),
+      recipe.string('description').notNullable(),
+      recipe.string('prep').notNullable(),
+      recipe.string('total').notNullable(),
+      recipe.string('servings').notNullable(),
+      recipe.string('yield').notNullable(),
+      recipe.string('nutritionInfo').notNullable(),
+      recipe.string('ingredients').notNullable()
+  })
+
+  .createTable('userRecipes', userRecipe => {
+    userRecipe.increments('userRecipeId'),
+    userRecipe.integer('userId')
+    .unsigned()
+    .notNullable()
+    .references('userId')
+    .inTable('users')
+    .onDelete('CASCADE')
+    .onUpdate('CASCADE'),
+    userRecipe.integer('recipeId')
+        .unsigned()
+        .notNullable()
+        .references('recipeId')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+  })
     
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users')
+  return knex.schema.dropTableIfExists('userRecipes')
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('users')
 };
