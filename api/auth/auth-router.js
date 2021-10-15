142cbb0c4ff87db8c078d9ec741e1f150e7617b9
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-10-14 19:24:47 -0600

    wip

---
+++ api/auth/auth-router.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const User = require('../users/usersModel');
+const {  } = require('../auth/');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mw = require('../recipes/middleware');
+const {  } = require('./')
+
+
