16b07935c3e7166247bf6ca71ba90e1ecd8db60b
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-10-14 20:39:56 -0600

    auth-router complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,13 +1,43 @@
 const router = require('express').Router();
 const User = require('../users/usersModel');
-const { jwtSecret } = require('../auth/secret');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const mw = require('../recipes/middleware');
-const { 
-    checkU
+const {
+    checkUsernameIsFree,
+    checkUsernameExists,
+    requirePassword
  } = require('./auth-middleware');
+ const tokenBuilder = require('./tokenbuilder');
 
 
-router.post('/register', mw)
+router.post('/register', mw, requirePassword, checkUsernameIsFree, async (req, res, next) => {
+    const credentials = req.body
+    try {
+        const hash = bcrypt.hashSync(credentials.password, 8)
+        credentials.password = hash
+        let user = await User.add(credentials)
+        const token = tokenBuilder(user)
+        res.status(201).json({ user, token })
+    } catch (err) {
+        next(err)
+    }
+})
 
+router.post('/login', mw, requirePassword, checkUsernameExists, async (req, res, next) => {
+    const { username, password } = req.body
+    try {
+        const [user] = await User.findBy({ username })
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = tokenBuilder(user)
+            res.status(200).json({
+                token: token
+            })
+        } else {
+            res.status(404).json({ message:  'Invalid login credentials' })
+        }
+    } catch (err) {
+        next({ status: 500, message:  'Error loggin in user', ...err })
+    }
+})
+
+module.exports = router;
