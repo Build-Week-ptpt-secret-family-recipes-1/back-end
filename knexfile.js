1ae8d8dbaf31ee81d171edaa7a7113e901d31d93
Kyle <kyle.e.cardwell@gmail.com>
2021-10-16 15:11:42 -0600

    installed postgres

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,69 @@
-// Update with your config settings.
-const common = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  seeds: { directory: './data/seeds' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-  connection: { filename: './data/familyRecipes.db3'}
+require('dotenv').config();
+
+const pg = require('pg')
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
+const sharedConfig = {
+  client: 'pg',
+  migrations: { directory: './api/data/migrations' },
+  seeds: { directory: './api/data/seeds' },
 }
 
 module.exports = {
-
   development: {
-    ...common,
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
   },
-
-  staging: {
-    // ...common,
-    // connection: {
-    //   database: 'my_db',
-    //   user:     'username',
-    //   password: 'password'
-    // },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-  },
-
-  production: {
-    ...common,
-  },
-
   testing: {
-    ...common,
-    connection: { filename: './data/test.db3' }
-  }
+    ...sharedConfig,
+    connection: process.env.TESTING_DATABASE_URL,
+  },
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
+  },
+}
 
-};
+
+// const common = {
+//   client: 'sqlite3',
+//   useNullAsDefault: true,
+//   migrations: { directory: './data/migrations' },
+//   seeds: { directory: './data/seeds' },
+//   pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+//   connection: { filename: './data/familyRecipes.db3'}
+// }
+
+// module.exports = {
+
+//   development: {
+//     ...common,
+//   },
+
+//   staging: {
+//     // ...common,
+//     // connection: {
+//     //   database: 'my_db',
+//     //   user:     'username',
+//     //   password: 'password'
+//     // },
+//     // pool: {
+//     //   min: 2,
+//     //   max: 10
+//     // },
+//   },
+
+//   production: {
+//     ...common,
+//   },
+
+//   testing: {
+//     ...common,
+//     connection: { filename: './data/test.db3' }
+//   }
+
+// };
